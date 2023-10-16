8036f7e335c75737bb1a36a2cf2aca8a63b520c6
Bazz9323 <147325215+Bazz9323@users.noreply.github.com>
2023-10-16 21:00:21 +0700

    Add files via upload

---
+++ Hari8_foreach.java
@@ -0,0 +1,8 @@
+public class Hari8_foreach {
+    public static void main(String[] args) {
+        int [] angka = {1,2,3,4,5};
+        for (int i : angka ) {
+            System.out.println(i);
+        }
+    }
+}
