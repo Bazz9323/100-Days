a19f930f443f64158d813485d49ed6c8d848d5ba
Baozz <147325215+Bazz9323@users.noreply.github.com>
2023-11-05 05:53:01 +0700

    Add files via upload

---
+++ Hari28_PolaSegitiga.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Hari28_PolaSegitiga {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan angka : ");
+        int a = input.nextInt();
+
+        for (int i = 1; i <=a; i++) {
+            System.out.println();
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+        }
+    }
+}
