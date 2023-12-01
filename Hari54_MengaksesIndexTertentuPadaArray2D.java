27874753b87c8f0fd59a23a542d070817e01c1d7
Baozz <147325215+Bazz9323@users.noreply.github.com>
2023-12-01 20:19:07 +0700

    Add files via upload

---
+++ Hari54_MengaksesIndexTertentuPadaArray2D.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Hari54_MengaksesIndexTertentuPadaArray2D {
+    public static void main(String[] args) {
+        int [][] angka = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println(angka[0][2]);
+    }
+}
