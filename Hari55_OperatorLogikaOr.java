8d45334df1016f48573af9c2931abea2d47e1d33
Baozz <147325215+Bazz9323@users.noreply.github.com>
2023-12-02 22:20:55 +0700

    Add files via upload

---
+++ Hari55_OperatorLogikaOr.java
@@ -0,0 +1,12 @@
+public class Hari55_OperatorLogikaOr {
+    public static void main(String[] args) {
+        boolean var1 = true;
+        boolean var2 = false;
+
+        if(var1 || var2){
+            System.out.println("Program berhasil");
+        }else{
+            System.out.println("Program gagal");
+        }
+    }
+}
