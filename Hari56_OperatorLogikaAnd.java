348302393178835f82df01d16bf7783df11753e6
Baozz <147325215+Bazz9323@users.noreply.github.com>
2023-12-03 21:47:23 +0700

    Add files via upload

---
+++ Hari56_OperatorLogikaAnd.java
@@ -0,0 +1,12 @@
+public class Hari56_OperatorLogikaAnd {
+    public static void main(String[] args) {
+        boolean var1 = true;
+        boolean var2 = true;
+
+        if(var1 && var2){
+            System.out.println("Program berhasil");
+        }else{
+            System.out.println("Program gagal");
+        }
+    }
+}
