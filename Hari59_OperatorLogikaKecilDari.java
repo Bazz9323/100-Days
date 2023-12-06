ae0a350dfbfec50a3b6ceba4bd422b5beab1caa1
Baozz <147325215+Bazz9323@users.noreply.github.com>
2023-12-06 22:14:06 +0700

    Add files via upload

---
+++ Hari59_OperatorLogikaKecilDari.java
@@ -0,0 +1,7 @@
+public class Hari59_OperatorLogikaKecilDari {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
