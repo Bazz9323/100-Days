f2ffba6bef7f789162f141b910bc47fb2be65770
Baozz <147325215+Bazz9323@users.noreply.github.com>
2023-12-08 23:00:45 +0700

    Add files via upload

---
+++ Hari61_ModulusPadaIf.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hari61_ModulusPadaIf {
+    public static void main(String[] args) {
+        int angka = 10;
+        if (angka%2==0){
+            System.out.println("ini genap");
+        }else {
+            System.out.println("ini ganjil");
+        }
+    }
+
+}
