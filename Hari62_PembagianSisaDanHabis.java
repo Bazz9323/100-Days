da8940c4cb051cdc4d4ea049926db106c99c569e
Baozz <147325215+Bazz9323@users.noreply.github.com>
2023-12-09 22:42:02 +0700

    Add files via upload

---
+++ Hari62_PembagianSisaDanHabis.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hari62_PembagianSisaDanHabis {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Angka : ");
+        double angka = input.nextDouble();
+        System.out.println(angka/2);
+        System.out.println(angka%2);
+    }
+}
