6bfbf08a32d4bf8c4660af7135e14ab5e7768d41
Baozz <147325215+Bazz9323@users.noreply.github.com>
2023-12-13 22:35:32 +0700

    Add files via upload

---
+++ Hari66_RandomAngka.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class Hari66_RandomAngka {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int angka = random.nextInt(1000);
+        System.out.println(angka);
+    }
+}
